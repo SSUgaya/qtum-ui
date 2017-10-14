7b57e28cb2026bd2f8ea446cfdf3136df209ca7e
ATJohnson <arubyrailsdev@gmail.com>
2017-10-15 03:34:17 +1100

    Created GET request and parse JSON response to the view

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
-
+import json
+import os
 app = Flask(__name__)
+
+def get_info():
+    p = os.popen("/home/pi/qtum-wallet/bin/qtum-cli getinfo").read()
+    parsed_json = json.loads(p)
+    return parsed_json
+
+def get_stake():
+    p = os.popen("/home/pi/qtum-wallet/bin/qtum-cli getstakinginfo").read()
+    parsed_json = json.loads(p)
+    return parsed_json
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', info_output=get_info(), stake_output=get_stake())
 
-@app.route('/cakes')
-def cakes():
-    return 'Yummy Cakes nom nom!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
