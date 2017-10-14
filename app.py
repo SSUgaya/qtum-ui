b815f2cc5dff5de132fa9c8c77b035e48da30e65
ATJohnson <arubyrailsdev@gmail.com>
2017-10-14 23:12:03 +1100

    Setup Bootstrap CDN

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
-import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html')
 
 @app.route('/cakes')
 def cakes():
-    return 'Yummy Cakes!'
+    return 'Yummy Cakes nom nom!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
