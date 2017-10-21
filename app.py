6cc2eba4e9118bbaaeda29b61580467156c41233
ATJohnson <arubyrailsdev@gmail.com>
2017-10-22 02:58:01 +1100

    Added wallet info and updated layout

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import json
 import os
+
 app = Flask(__name__)
 
 def get_info():
@@ -13,9 +14,15 @@ def get_stake():
     parsed_json = json.loads(p)
     return parsed_json
 
+def get_time():
+    p = os.popen("/home/pi/qtum-wallet/bin/qtum-cli getstakinginfo | grep expectedtime | cut -d':' -f2").read()
+    time = int(p) / 60 / 60 / 24
+    expected_stake = round(time)
+    return expected_stake
+
 @app.route('/')
 def index():
-    return render_template('index.html', info_output=get_info(), stake_output=get_stake())
+    return render_template('index.html', info_output=get_info(), stake_output=get_stake(), stake_time=get_time())
 
 
 if __name__ == '__main__':
