e8397df3381f7f6501395fd76f5a587ed13a2ec3
ATJohnson <arubyrailsdev@gmail.com>
2017-10-22 20:33:03 +1100

    Started work on the wallet Send function

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 import os
+import subprocess
+
 
 app = Flask(__name__)
 
@@ -40,6 +42,22 @@ def transaction():
 def contract():
     return render_template('contract.html', info_output=get_info(), stake_output=get_stake(), stake_time=get_time())
 
+@app.route('/send_qtum', methods=['POST'])
+def send_qtum():
+    address = request.form['qtumAddress']
+    amount = request.form['qtumAmount']
+    passwd = request.form['qtumPass']
+    passwd_time = '60'
+    unlock = ['/home/pi/qtum-wallet/bin/qtum-cli', 'walletpassphrase']
+    unlock.append(passwd)
+    unlock.append(passwd_time)
+    subprocess.run(unlock)
+    command = ['/home/pi/qtum-wallet/bin/qtum-cli', 'sendtoaddress']
+    command.append(address)
+    command.append(amount)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+    (out,err) = process.communicate()
+    return out
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
