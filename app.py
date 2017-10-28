983433b54a22af9c32b007d5dbe5818035e7e28b
ATJohnson <arubyrailsdev@gmail.com>
2017-10-29 03:57:30 +1100

    Added transactions list for last 5 TX on index page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, BooleanField, PasswordField
 from wtforms.validators import InputRequired, Length
+import time
 import json
 import os
 import subprocess
@@ -39,9 +40,15 @@ def get_pending():
     parsed_json = json.loads(p)
     return parsed_json
 
+def get_last_tx():
+    p = os.popen("/users/Boss/qtum-wallet/bin/qtum-cli listtransactions").read()
+    parsed_json = json.loads(p)
+    return parsed_json
+
 @app.route('/')
 def index():
-    return render_template('index.html', info_output=get_info(), stake_output=get_stake(), stake_time=get_time(), pending_balance=get_pending())
+    date = time
+    return render_template('index.html', last_tx=get_last_tx(), info_output=get_info(), stake_output=get_stake(), stake_time=get_time(), pending_balance=get_pending(), **locals())
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
@@ -64,7 +71,7 @@ def send():
         command.append(label)
         process = subprocess.Popen(command, stdout=subprocess.PIPE,stdin=subprocess.PIPE)
         (out,err) = process.communicate()
-        return out
+        return render_template('send.html', out)
 
     return render_template('send.html', form=form, info_output=get_info(), stake_output=get_stake(), stake_time=get_time())
 
