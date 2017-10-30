45de4dcd5d8466984dd051c1276a83f374c22552
ATJohnson <arubyrailsdev@gmail.com>
2017-10-31 01:17:20 +1100

    Update blance using getwalletinfo

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class SendForm(FlaskForm):
     feeAmount = BooleanField('fee')
 
 def get_info(): # On Pi working directory is "/home/pi/qtum-wallet/bin/qtum-cli getinfo"
-    p = os.popen("/users/Boss/qtum-wallet/bin/qtum-cli getinfo").read()
+    p = os.popen("/users/Boss/qtum-wallet/bin/qtum-cli getwalletinfo").read()
     parsed_json = json.loads(p)
     return parsed_json
 
@@ -35,11 +35,6 @@ def get_time():
     expected_stake = round(time)
     return expected_stake
 
-def get_pending():
-    p = os.popen("/users/Boss/qtum-wallet/bin/qtum-cli getunconfirmedbalance").read()
-    parsed_json = json.loads(p)
-    return parsed_json
-
 def get_last_tx():
     p = os.popen("/users/Boss/qtum-wallet/bin/qtum-cli listtransactions").read()
     parsed_json = json.loads(p)
@@ -48,7 +43,7 @@ def get_last_tx():
 @app.route('/')
 def index():
     date = time
-    return render_template('index.html', last_tx=get_last_tx(), info_output=get_info(), stake_output=get_stake(), stake_time=get_time(), pending_balance=get_pending(), **locals())
+    return render_template('index.html', last_tx=get_last_tx(), info_output=get_info(), stake_output=get_stake(), stake_time=get_time(), **locals())
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
@@ -73,7 +68,7 @@ def send():
         command.append(label)
         process = subprocess.Popen(command, stdout=subprocess.PIPE,stdin=subprocess.PIPE)
         (out,err) = process.communicate()
-        return render_template('send.html', out)
+        return out
 
     return render_template('send.html',last_tx=get_last_tx(), info_output=get_info(), stake_output=get_stake(), stake_time=get_time(), **locals())
 
